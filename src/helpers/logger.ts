6d1f9d25ba3778ab0ca749a40301bdfe8d7b4ca0
Mayank <mayankrajput8745@gmail.com>
2023-09-23 17:48:31 +0100

    feat(logger is added): winstor logger is added

---
+++ src/helpers/logger.ts
@@ -0,0 +1,26 @@
+import { createLogger, transports, format } from "winston";
+
+const logLevel = process.env.NODE_ENV === "production" ? "verbose" : "debug";
+
+const consoleTransports = new transports.Console({
+  level: logLevel,
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf((info) => {
+      if (info.stack)
+        return `${info.timestamp} ${info.level} ${info.message} \n ${info.stack}`;
+      if (info.name === "API_ERROR")
+        return `${info.timestamp} ${info.level}: ${info.type}`;
+      return `${info.timestamp} ${info.level}: ${info.message}`;
+    })
+  ),
+});
+
+const winstonLogger = createLogger({
+  transports: consoleTransports,
+  exceptionHandlers: consoleTransports,
+  exitOnError: false,
+});
+
+export const logger = winstonLogger;
