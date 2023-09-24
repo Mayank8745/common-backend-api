8bca3854f0064ae36ef9f2a472fa96baa4107aa6
Mayank <mayankrajput8745@gmail.com>
2023-09-24 15:05:30 +0100

    feat(api logger is added and clean project): clean command is added and api logger with all logs

---
+++ src/middlewares/apiLogger.ts
@@ -0,0 +1,39 @@
+import { transports, format } from "winston";
+import { logger } from "express-winston";
+
+export const apiLogger = logger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(({ message, level, timestamp, stack }) => {
+      if (stack) return `${timestamp} ${level}: ${message} \n ${stack}`;
+      return `${timestamp} ${level}: ${message}`;
+    })
+  ),
+  meta: false,
+  colorize: true,
+  statusLevels: true,
+  skip: (req, _res) => req.method === "OPTIONS",
+});
+
+export const errorLogger = logger({
+  transports: [new transports.Console()],
+  format: format.combine(
+    format.errors({ stack: true }),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf(({ message, level, timestamp, meta }) => {
+      const error = JSON.stringify(meta?.error);
+      const username = JSON.stringify(meta?.username);
+      console.error({ timestamp, level, message, meta });
+      return `${timestamp} ${level}: ${message}, username: ${username}, errors: ${error}`;
+    })
+  ),
+  colorize: true,
+  meta: true,
+  requestField: null,
+  dynamicMeta: (req, _res, next) => {
+    return { url: req.url, body: req.body };
+  },
+  skip: (req, _res) => req.method === "OPTIONS",
+});
